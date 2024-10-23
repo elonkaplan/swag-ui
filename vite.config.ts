29d4dc45b4504a0a37c267f8672cfbf952a948ce
Ilya Kaplan <ik@alphaguilty.io>
2024-10-23 03:23:01 -0400

    fix(app): fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,32 @@
-import build from '@hono/vite-build/cloudflare-pages'
-import devServer from '@hono/vite-dev-server'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import { defineConfig } from 'vite'
+import adapter from "@hono/vite-dev-server/cloudflare";
+import build from "@hono/vite-build/cloudflare-pages";
+import { defineConfig } from "vite";
+import devServer from "@hono/vite-dev-server";
 
-export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx'
-    })
-  ]
-})
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      esbuild: {
+        jsxImportSource: "hono/jsx/dom", // Optimized for hono/jsx/dom
+      },
+      build: {
+        rollupOptions: {
+          input: "./src/client.tsx",
+          output: {
+            entryFileNames: "static/client.js",
+          },
+        },
+      },
+    };
+  } else {
+    return {
+      plugins: [
+        build(),
+        devServer({
+          adapter,
+          entry: "src/index.tsx",
+        }),
+      ],
+    };
+  }
+});
